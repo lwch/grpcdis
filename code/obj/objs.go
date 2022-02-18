e8423ffc8af772d22b61ea4d30f89d47d2ea7959
lwch <lwch748@gmail.com>
2022-02-18 14:41:49 +0800

    去除哈希

--- code/obj/objs.go
+++ code/obj/objs.go
@@ -2,7 +2,7 @@ package obj
 
 import "sync"
 
-const blocks = 1024
+const blocks = 255
 
 // Objs objects
 type Objs struct {
@@ -30,7 +30,7 @@ func hash(str string) uint64 {
 
 // Set set object
 func (objs *Objs) Set(key string, value *Obj) {
-	idx := hash(key) % blocks
+	idx := uint8(key[0]) % blocks
 	objs.locks[idx].Lock()
 	objs.data[idx][key] = value
 	objs.locks[idx].Unlock()
@@ -38,7 +38,7 @@ func (objs *Objs) Set(key string, value *Obj) {
 
 // Lookup lookup object
 func (objs *Objs) Lookup(key string) *Obj {
-	idx := hash(key) % blocks
+	idx := uint8(key[0]) % blocks
 	objs.locks[idx].RLock()
 	defer objs.locks[idx].RUnlock()
 	return objs.data[idx][key]
