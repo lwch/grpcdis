6312605f9abf72d1b32033d320ab78401ed248e6
lwch <lwch748@gmail.com>
2022-02-18 14:27:44 +0800

    support sectional lock

--- code/obj/objs.go
+++ code/obj/objs.go
@@ -2,27 +2,44 @@ package obj
 
 import "sync"
 
+const blocks = 1024
+
 // Objs objects
 type Objs struct {
-	sync.RWMutex
-	data map[string]*Obj // TODO: change to hash struct
+	locks [blocks]sync.RWMutex
+	data  [blocks]map[string]*Obj
 }
 
 // New new objects
 func New() *Objs {
-	return &Objs{data: make(map[string]*Obj)}
+	ret := new(Objs)
+	for i := 0; i < blocks; i++ {
+		ret.data[i] = make(map[string]*Obj)
+	}
+	return ret
+}
+
+// Bernstein hash
+func hash(str string) uint64 {
+	hash := uint64(5381)
+	for i := 0; i < len(str); i++ {
+		hash = (hash << 5) + hash + uint64(str[i])
+	}
+	return hash
 }
 
 // Set set object
 func (objs *Objs) Set(key string, value *Obj) {
-	objs.Lock()
-	objs.data[key] = value
-	objs.Unlock()
+	idx := hash(key) % blocks
+	objs.locks[idx].Lock()
+	objs.data[idx][key] = value
+	objs.locks[idx].Unlock()
 }
 
 // Lookup lookup object
 func (objs *Objs) Lookup(key string) *Obj {
-	objs.RLock()
-	defer objs.RUnlock()
-	return objs.data[key]
+	idx := hash(key) % blocks
+	objs.locks[idx].RLock()
+	defer objs.locks[idx].RUnlock()
+	return objs.data[idx][key]
 }
