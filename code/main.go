170ec6e42a109c74e5ec3b9e7f1cc42c013a9605
lwch <lwch748@gmail.com>
2022-02-18 15:41:31 +0800

    add debug code

--- code/main.go
+++ code/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 
+	"net/http"
 	_ "net/http/pprof"
 
 	"github.com/lwch/goredis/code/app"
@@ -12,6 +13,9 @@ import (
 func main() {
 	port := flag.Int("port", 6379, "listen port")
 	flag.Parse()
+
+	// for pprof
+	go http.ListenAndServe(":9999", nil)
 
 	a := app.New()
 	runtime.Assert(a.ListenAndServe(uint16(*port)))
